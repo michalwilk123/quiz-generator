8a743ca61fce1fb2baa738d4853945e478b61818
michalwilk <michal.wilk0@yahoo.com>
2022-06-18 22:00:35 +0200

    add question amounts

--- src/components/SelectQuiz.tsx
+++ src/components/SelectQuiz.tsx
@@ -8,6 +8,9 @@ import {
   WrapItem,
   Button,
   FormControl,
+  RadioGroup,
+  Radio,
+  Flex,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { createSearchParams, useNavigate } from "react-router-dom";
@@ -36,16 +39,25 @@ export enum QuizOptions {
   TIMER = "Show quiz timer",
 }
 
+export enum QuizQuestionAmounts {
+  FIVE = "5",
+  FIFTEEN = "15",
+  TWENTY = "20",
+  ALL = "All",
+}
+
 type Props = {};
 
 const MultiCheckboxWrap = ({
   options,
   chosenOptions,
   setChosenOptions,
+  defaultChecked,
 }: {
   options: string[];
   chosenOptions: string[];
   setChosenOptions: (opt: string[]) => any;
+  defaultChecked?: boolean;
 }) => {
   const handleOptChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.toString();
@@ -66,7 +78,11 @@ const MultiCheckboxWrap = ({
     <Wrap mt="15px" mx="30px" spacing="13px">
       {options.map((opt, idx) => (
         <WrapItem key={idx}>
-          <Checkbox value={idx} onChange={handleOptChange}>
+          <Checkbox
+            value={idx}
+            onChange={handleOptChange}
+            defaultChecked={defaultChecked}
+          >
             <Box color="whiteAlpha.800">{opt}</Box>
           </Checkbox>
         </WrapItem>
@@ -80,6 +96,9 @@ const SelectQuiz = (props: Props) => {
   const [configuration, setConfiguration] = useState<string[]>([]);
   const [questionTypes, setQuestionTypes] = useState<string[]>([]);
   const [chosenQuizUrl, setChosenQuizUrl] = useState<string>("");
+  const [questionAmount, setQuestionAmount] = useState<string>(
+    QuizQuestionAmounts.ALL
+  );
 
   const handleOpenQuiz = (event: React.FormEvent) => {
     const urlParameters = {
@@ -125,13 +144,38 @@ const SelectQuiz = (props: Props) => {
         </Box>
         <Box width="80%">
           <Heading textColor="whiteAlpha.600" size="md">
-            Show quiz types
+            Show question types
           </Heading>
           <MultiCheckboxWrap
             options={Object.values(QuestionTypes) as string[]}
             setChosenOptions={setQuestionTypes}
             chosenOptions={questionTypes}
+            defaultChecked
           />
+        </Box>
+        <Box width="80%">
+          <Heading textColor="whiteAlpha.600" size="md">
+            Select numer of questions
+          </Heading>
+          <RadioGroup
+            width="100%"
+            onChange={setQuestionAmount}
+            defaultValue={questionAmount}
+          >
+            <Wrap mt="15px" mx="30px" spacing="13px">
+              {(Object.values(QuizQuestionAmounts) as string[]).map(
+                (val, idx) => {
+                  return (
+                    <WrapItem key={idx}>
+                      <Flex as="label" color="whiteAlpha.800">
+                        <Radio value={val}>{val}</Radio>
+                      </Flex>
+                    </WrapItem>
+                  );
+                }
+              )}
+            </Wrap>
+          </RadioGroup>
         </Box>
         <Button disabled={!chosenQuizUrl} onClick={handleOpenQuiz} size="lg">
           Generate quiz!
