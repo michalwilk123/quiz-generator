376e7ab2c0e128ddf20b8d054fa9baafff7488df
michalwilk <michal.wilk0@yahoo.com>
2022-06-18 22:10:37 +0200

    fix base path

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,6 +1,7 @@
 import { Center, Heading } from "@chakra-ui/react";
 
 const NotFound = () => {
+  console.log(`Current path location: ${window.location.pathname}`);
   return (
     <Center width="100%" mt="20px">
       <Heading color="whiteAlpha.800" size="3xl">
