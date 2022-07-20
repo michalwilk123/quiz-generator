e87bdce5eb6015fa9b7986382019f82dfaed7ea0
michalwilk <michal.wilk0@yahoo.com>
2022-07-20 18:13:48 +0200

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+set -e
 yarn build
+TMP_QUIZES=$(mktemp)
+TMP_CONFIG=$(mktemp)
+tar cf "$TMP_QUIZES" src/quizes/*
+mv src/quiz_config_ts "$TMP_CONFIG"
+
 git checkout gh-pages
+tar xf "$TMP_QUIZES"
+mv "$TMP_CONFIG" quiz_config_ts
+
 git add -A
 git commit -m "Update pages: $(date -u +"%Y-%m-%d_%H:%M")"
 git push origin gh-pages
 git checkout master
 
+rm "$TMP_QUIZES" "$TMP_CONFIG"
+set -x
