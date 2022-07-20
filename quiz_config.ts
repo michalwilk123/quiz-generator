ef703e8f2c1b2a634001170f696f3f315982037a
michalwilk <michal.wilk0@yahoo.com>
2022-07-20 18:43:40 +0200

    Update pages: 2022-07-20_16:43

---
+++ quiz_config.ts
@@ -0,0 +1,14 @@
+const quizConfig: any[] = [
+  {
+    filename: "sieciTel.json",
+    name: "Sieci telekomunikacyjne",
+    urlName: "1",
+  },
+  {
+    filename: "isp.json",
+    name: "Interaktywne Systemy Programowalne",
+    urlName: "2",
+  },
+];
+
+export default quizConfig;
