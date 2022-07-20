588746b853d7a3b6de591c4981037b96fa339486
michalwilk <michal.wilk0@yahoo.com>
2022-07-20 18:43:30 +0200

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,20 +5,27 @@ set -e
 yarn build
 TMP_QUIZES=$(mktemp)
 TMP_CONFIG=$(mktemp)
-tar cf "$TMP_QUIZES" src/quizes/*
+cd src
+tar cf "$TMP_QUIZES" quizes/*
+cd ..
 cp src/quiz_config.ts "$TMP_CONFIG"
 
 git checkout gh-pages
-tar xf "$TMP_QUIZES"
-mv "$TMP_CONFIG"  quiz_config.ts
 
-cat "$TMP_QUIZES"
-exit
+tar xf "$TMP_QUIZES"
+rm -r assets
+yes | cp -r dist/* .
+cp "$TMP_CONFIG" quiz_config.ts
 
 git add -A
-git commit -m "Update pages: $(date -u +"%Y-%m-%d_%H:%M")"
-git push origin gh-pages
+git diff --cached --exit-code && git commit -m "Update pages: $(date -u +"%Y-%m-%d_%H:%M")" \
+    && git push origin gh-pages && echo "Successfully updated your github site!" \
+    2> /dev/null
 git checkout master
+tar xf "$TMP_QUIZES"
+rm -r src/quizes
+mv quizes src
+cp "$TMP_CONFIG"  src/quiz_config.ts
 
-rm "$TMP_QUIZES"
+rm "$TMP_QUIZES" "$TMP_CONFIG"
 set -x
