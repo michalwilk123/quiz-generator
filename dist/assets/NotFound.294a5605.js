da933bbb36d47df602bb423697b67d5b4f3bc8ff
michalwilk <michal.wilk0@yahoo.com>
2022-07-20 15:29:32 +0200

    remove dist folder

--- dist/assets/NotFound.294a5605.js
+++
@@ -1,1 +0,0 @@
-import{k as t,C as e,H as o}from"./index.9f88a317.js";const d=()=>(document.title="404 - not found",t(e,{width:"100%",mt:"20px",children:t(o,{color:"whiteAlpha.800",size:"3xl",children:"404 Page not Found"})}));export{d as default};
