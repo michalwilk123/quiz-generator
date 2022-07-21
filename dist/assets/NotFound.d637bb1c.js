b40e44d03e2f5edb20f9eb6e8365ae8d8beb79f5
michalwilk <michal.wilk0@yahoo.com>
2022-07-21 12:59:41 +0200

    create build

---
+++ dist/assets/NotFound.d637bb1c.js
@@ -0,0 +1,1 @@
+import{k as t,C as e,H as o}from"./index.aabe6e39.js";const d=()=>(document.title="404 - not found",t(e,{width:"100%",mt:"20px",children:t(o,{color:"whiteAlpha.800",size:"3xl",children:"404 Page not Found"})}));export{d as default};
