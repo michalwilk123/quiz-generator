b946cba7ff1bc5fdf9e1b1938fa2939daadd17b6
Michał Wilk <michal.wilk0@yahoo.com>
2022-12-17 17:01:13 +0100

    include quizes

---
+++ src/quiz_config.ts
@@ -0,0 +1,19 @@
+const quizConfig: any[] = [
+  {
+    filename: "sieciTel.json",
+    name: "Sieci telekomunikacyjne",
+    urlName: "st",
+  },
+  {
+    filename: "isp.json",
+    name: "Interaktywne Systemy Programowalne",
+    urlName: "isp",
+  },
+  {
+    filename: "zsbd.json",
+    name: "Zarządzanie Systemami Baz Danych",
+    urlName: "zsbd",
+  },
+];
+
+export default quizConfig;
